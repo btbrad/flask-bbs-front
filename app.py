934eb928dd902428b0fc534024f13d8fb74cd60b
btbrad <biantaobrad@gmail.com>
2025-09-17 23:12:18 +0800

    feat: 登录注册页面模板渲染

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import config
 from exts import db
 from flask_migrate import Migrate
 from models import auth
+from apps.front import front_bp
 
 app = Flask(__name__)
 
@@ -12,11 +13,8 @@ db.init_app(app)
 
 migrate = Migrate(app, db)
 
-
-@app.route("/")
-def hello_world():
-    return "Hello World!"
-
+# 注册蓝图
+app.register_blueprint(front_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ apps/front/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, request
+
+bp = Blueprint("front", __name__, url_prefix="/")
+
+
+@bp.route("/")
+def hello_world():
+    return "Hello World!"
+
+
+@bp.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("front/login.html")
+
+
+@bp.route("/register/", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("/front/register.html")
