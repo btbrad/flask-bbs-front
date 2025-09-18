f037f3032bd853cb157108a819f7531dffd32c3c
btbrad <biantaobrad@gmail.com>
2025-09-18 21:00:55 +0800

    '邮箱验证码发送'

--- apps/front/views.py
+++ apps/front/views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request
+import random
+import string
+from flask import Blueprint, jsonify, render_template, request
+from flask_mail import Message
+from exts import mail
 
 bp = Blueprint("front", __name__, url_prefix="/")
 
@@ -6,6 +10,27 @@ bp = Blueprint("front", __name__, url_prefix="/")
 @bp.route("/")
 def hello_world():
     return "Hello World!"
+
+
+@bp.get("/email/captcha/")
+def email_captcha():
+    email = request.args.get("email")
+    if not email:
+        return jsonify({"code": 400, "message": "请输入邮箱"})
+    source = list(string.digits)
+    captcha = "".join(random.sample(source, 6))
+    message = Message(
+        subject="flask_bbs注册邮箱验证",
+        body=f"您的验证码是：{captcha}",
+        recipients=[email],
+    )
+    try:
+        mail.send(message)
+    except Exception as e:
+        print("邮件发送失败")
+        print(e)
+        return jsonify({"code": 500, "message": "邮件发送失败"})
+    return jsonify({"code": 200, "message": "邮件发送成功"})
 
 
 @bp.route("/login/", methods=["GET", "POST"])
